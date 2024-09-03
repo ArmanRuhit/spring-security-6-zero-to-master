c895d195872c924d88b86b4c3c51e01707390d37
arman <armanruhit09@gmail.com>
2024-09-03 14:40:37 +0600

    init

---
+++ src/main/java/com/dohatec/oms/springsecurity6zerotomaster/SpringSecurity6ZeroToMasterApplication.java
@@ -0,0 +1,13 @@
+package com.dohatec.oms.springsecurity6zerotomaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity6ZeroToMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurity6ZeroToMasterApplication.class, args);
+    }
+
+}
